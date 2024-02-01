dec6c09f606f03706fb50ac321021cb88cbfb02e
Leonardo Emanuel Zambrano Lopez <ZambranoLopez.LeonardoEmanuel@swm.de>
2024-02-01 11:47:39 +0100

    cleanup

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
-from fastapi import FastAPI, UploadFile, HTTPException
-from starlette.responses import FileResponse, StreamingResponse
+from fastapi import FastAPI, UploadFile
+from starlette.responses import FileResponse
 from starlette.staticfiles import StaticFiles
-import io
-from transfer import *
-from querier import Querier
+from db_interface import DBInterface
 
 # Create a FastAPI app
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
-transfer = Transfer(
-    ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"],
-    "sqlite:///your_database.db",
-    "flash",
-    "orders",
-)
 
-querier = Querier("sqlite:///your_database.db", "flash", "orders")
+db_interface = DBInterface("sqlite:///your_database.db")
 
 
-# Route to get entries based on a SQL query
+@app.on_event("startup")
+def startup():
+    db_interface.initialize_database()
+
+
 @app.get("/users/{user}/")
 async def query_user(user):
-    return querier.get_user_infos(user).to_dict(orient="records")
+    return db_interface.get_user_infos(user).to_dict(orient="records")
 
 
 @app.get("/panels/{panel}/")
 async def query_serial_number(panel):
-    return querier.get_panel_info(panel).to_dict(orient="records")
+    return db_interface.get_panel_info(panel).to_dict(orient="records")
 
 
 @app.get("/users/{user}/download")
 async def download_user(user):
-    df = querier.get_user_infos(user)
-    return transfer.download_df(df, user)
+    df = db_interface.get_user_infos(user)
+    return db_interface.download_df(df, user)
 
 
 @app.get("/panels/{panel}/download")
 async def download_panel(panel):
-    df = querier.get_panel_info(panel)
-    return transfer.download_df(df, panel)
+    df = db_interface.get_panel_info(panel)
+    return db_interface.download_df(df, panel)
 
 
 @app.get("/")
@@ -48,9 +44,9 @@ async def query():
 
 @app.post("/upload_orders/")
 async def upload_orders(file: UploadFile):
-    transfer.import_order_infos(file.file)
+    db_interface.import_order_infos(file.file)
 
 
 @app.post("/upload_flash/")
 async def upload_flash(file: UploadFile):
-    transfer.import_flash_infos(file.file)
+    db_interface.import_flash_infos(file.file)
